6adbec7f0058dfedf61de9e56eabd87b2f92307e
Galen Rice <griceturrble@protonmail.com>
2024-12-25 01:18:49 -0500

    chore: unused comment removed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,8 +15,3 @@ export default {
     require('@tailwindcss/line-clamp'),
   ],
 }
-
-// TODO the current setup with postcss is getting a bit complex
-// I want to watch all the content folders for changes,
-// but postcss is likely not aware of them.
-// Probably just need the tailwind CLI and no other bull.
