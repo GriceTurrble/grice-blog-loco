4a6508678078696e6f4c37c9b97081250416c23a
Galen Rice <griceturrble@protonmail.com>
2024-12-27 23:40:34 -0500

    chore: docstring

--- src/initializers/custom_filters.rs
+++ src/initializers/custom_filters.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use markdown;
 use tera::{from_value, to_value, Error, Result, Value};
 
+/// Tera template filter `markdown`, for converting Markdown-formatted prose to HTML.
 pub fn filter_markdown(value: &Value, _: &HashMap<String, Value>) -> Result<Value> {
     let val = match from_value::<String>(value.clone()) {
         Ok(s) => s,
